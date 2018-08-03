21363003d5823ca21b8a6be34feeb2d7721f6620
Alan Taranti <alan.taranti@gmail.com>
2018-08-03 13:09:06 -0400

    criado componente de botão de ação

---
+++ src/components/ButtonAction.vue
@@ -0,0 +1,31 @@
+<template>
+    <v-btn
+            fixed
+            bottom
+            right
+            dark
+            fab
+            color="accent"
+            @click="botaoClicado">
+        <v-icon>{{ icone }}</v-icon>
+    </v-btn>
+</template>
+
+<script>
+    export default {
+      name: 'button-action',
+      props: ['icone'],
+      methods: {
+        botaoClicado() {
+          this.$emit('botao-clicado');
+        },
+      },
+    };
+</script>
+
+<style scoped>
+  .v-btn--floating .v-icon {
+    height: unset;
+    width: inherit;
+  }
+</style>
