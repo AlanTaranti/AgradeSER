5e5962b09f4c448c0be05d0dbd71012650f05f6d
Alan Taranti <alan.taranti@gmail.com>
2018-08-03 17:02:58 -0400

    adicionado views relato e datepicker para data

---
+++ src/views/autenticado/Relato.vue
@@ -0,0 +1,84 @@
+<template>
+  <v-container fluid>
+
+    <!-- Date Picker -->
+    <v-flex xs12 sm6 md4>
+      <v-menu
+        v-model="models.menuDatePicker"
+        absolute
+        offset-y
+        lazy
+        origin="center center"
+        transition="scale-transition"
+        style="width: 100%;">
+
+        <v-card slot="activator" style="width: 100%;">
+          <v-layout justify-space-between row fill-height>
+
+            <!-- Dia do Mês -->
+            <v-flex xs3>
+              <v-card-text align-center class="center-content-vh" style="height: 100%;">
+                <span>{{ diaDoRelato }}</span>
+              </v-card-text>
+            </v-flex>
+
+            <v-flex xs9>
+              <v-card-text class="text-xs-left">
+                <!-- Dia da Semana-->
+                <span class="text-xs-left">{{ diaDaSemanaDoRelato }}</span>
+                <br>
+                <!-- Mẽs e Ano -->
+                <span class="text-xs-left">{{ mesEAnoDoRelato  }}</span>
+              </v-card-text>
+            </v-flex>
+          </v-layout>
+        </v-card>
+
+        <v-date-picker v-model="models.datePicker"></v-date-picker>
+
+      </v-menu>
+    </v-flex>
+
+
+  </v-container>
+</template>
+
+<script>
+
+  import moment from 'moment';
+
+  moment.locale('pt-br');
+
+
+  export default {
+    name: "Relato",
+    data: () => ({
+      models: {
+        datePicker: moment().format('YYYY-MM-DD'),
+        menuDatePicker: false,
+      }
+    }),
+
+    computed: {
+      diaDoRelato() {
+        return moment(this.models.datePicker).format('DD');
+      },
+      diaDaSemanaDoRelato() {
+        return moment(this.models.datePicker).format('dddd');
+      },
+      mesEAnoDoRelato() {
+        return moment(this.models.datePicker).format('MMMM YYYY');
+      },
+    },
+  }
+</script>
+
+<style scoped>
+
+  .center-content-vh {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+</style>
