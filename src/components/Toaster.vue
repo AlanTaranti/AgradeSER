965c26968a6827a6d8a4596e57723879e16611d7
Alan Taranti <alan.taranti@gmail.com>
2018-08-29 15:29:32 -0400

    adicionado toaster

---
+++ src/components/Toaster.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-snackbar
+    :timeout=3000
+    :color="toasterColor"
+    v-model="toasterMostrar">
+    <b>{{ toasterMensagem }}</b>
+  </v-snackbar>
+</template>
+
+<script>
+
+  import store from '../vuex/store';
+
+  export default {
+    name: 'toaster',
+    store: store,
+    computed: {
+      toasterMostrar: {
+        set(toasterMostrar) {
+          this.$store.commit('toasterMostrar', toasterMostrar);
+        },
+        get() {
+          return this.$store.getters.toasterMostrar;
+        }
+      },
+      toasterMensagem: {
+        set(toasterMensagem) {
+          this.$store.commit('toasterMensagem', toasterMensagem);
+        },
+        get() {
+          return this.$store.getters.toasterMensagem;
+        }
+      },
+      toasterColor: {
+        set(toasterColor) {
+          this.$store.commit('toasterColor', toasterColor);
+        },
+        get() {
+          return this.$store.getters.toasterColor;
+        }
+      },
+    },
+  };
+</script>
+
+<style scoped>
+</style>
