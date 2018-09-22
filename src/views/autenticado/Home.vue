1f1e2ac96b5fae2c56382434b0941f9980adab8e
Alan Taranti <alan.taranti@gmail.com>
2018-09-22 16:09:21 -0400

    adicionado opção de esvaziar lixeira

--- src/views/autenticado/Home.vue
+++ src/views/autenticado/Home.vue
@@ -1,5 +1,27 @@
 <template>
   <v-container fluid style="padding: 0;">
+
+    <!-- Dialog -->
+    <v-dialog v-model="dialogQuestion.show" max-width="290">
+      <v-card raised hover color="white" v-if="dialogQuestion.show">
+        <v-card-title primary-title>
+          <div v-if="dialogQuestion.titleText" class="headline grey--text text--darken-3">
+            {{dialogQuestion.titleText}}
+          </div>
+        </v-card-title>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="error" flat="flat" @click.native="dialogQuestion.actionButtonFunction()">
+            {{dialogQuestion.actionButtonText}}
+          </v-btn>
+          <v-btn color="success" flat="flat" @click.native="dialogQuestion.show = false">
+            {{dialogQuestion.cancelButtonText}}
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
     <template v-for="relato in relatos">
       <v-flex xs12>
         <relato-card class="mb-3" :id="relato.id" :titulo="relato.titulo" :conteudo="relato.conteudo"
@@ -36,6 +58,14 @@
       dbRefs: {
         relatosRef: null,
       },
+      dialogQuestion: {
+        show: false,
+        titleText: 'Deseja mover o relato para a lixeira?',
+        cancelButtonText: 'Cancelar',
+        actionButtonText: 'Lixeira',
+        actionButtonFunction: this.removerRelato,
+      },
+
     }),
 
     computed: {
@@ -53,15 +83,52 @@
 
       fabFunction() {
         if (this.$route.path === '/lixeira') {
-
+          this.dialogQuestion.titleText = 'Deseja esvaziar a lixeira?';
+          this.dialogQuestion.actionButtonFunction = this.excluirTodosRelatosPermanentemente;
+          this.dialogQuestion.actionButtonText = 'Excluir';
+          this.dialogQuestion.show = true;
         }
         else {
           this.criarRelato();
         }
       },
 
+      excluirTodosRelatosPermanentemente() {
+        let counter = 0;
+        const quantidade = this.relatos.length;
+        const self = this;
+
+        this.dialogQuestion.show = false;
+
+        this.relatos.forEach(function (relato) {
+          self.dbRefs.relatosRef.doc(relato.id).delete()
+            .then(function () {
+              counter++;
+              if (counter === quantidade) {
+                self.$store.commit('toasterMensagem', 'Relatos excluidos permanentemente com sucesso!');
+                self.$store.commit('toasterColor', 'success');
+                self.$store.commit('toasterMostrar', true);
+              }
+            })
+            .catch(function () {
+              self.$store.commit('toasterMensagem', 'Tente novamente mais tarde');
+              self.$store.commit('toasterColor', 'error');
+              self.$store.commit('toasterMostrar', true);
+            })
+        })
+
+      },
+
       criarRelato() {
         this.$router.push({name: 'relato'});
+      },
+
+      inicializar(){
+        /* Firestore References */
+        const userRef = db.collection('usuario').doc(firebase.auth().currentUser.uid);
+        this.dbRefs.relatosRef = userRef.collection('relatos');
+
+        this.loadRelatos();
       },
 
       loadRelatos() {
@@ -108,16 +175,12 @@
 
     watch: {
       $route(to, from) {
-        this.loadRelatos();
+        this.inicializar();
       }
     },
 
     created() {
-      /* Firestore References */
-      const userRef = db.collection('usuario').doc(firebase.auth().currentUser.uid);
-      this.dbRefs.relatosRef = userRef.collection('relatos');
-
-      this.loadRelatos();
+      this.inicializar();
     }
   }
 </script>
