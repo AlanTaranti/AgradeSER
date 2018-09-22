d3ff8c4dc9c49bd01a4c8ead22d3d099254da6db
Alan Taranti <alan.taranti@gmail.com>
2018-09-22 14:29:32 -0400

    estilizado telas de login/singup

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -1,17 +1,100 @@
 <template>
-  <div>
-    <p>Vamos criar uma nova conta!</p>
-    <input type="text" v-model="email" placeholder="Email">
-    <br>
-    <input type="password" v-model="password" placeholder="Senha">
-    <br>
-    <button v-on:click="signUp">Cadastre-se</button>
-    <span>or go back to <router-link :to="{ name: 'login' }">login</router-link>.</span>
-  </div>
+  <v-container fluid :style="{backgroundImage: 'url('+background+')'}">
+    <!-- MD and UP -->
+    <v-layout align-center justify-center fill-height hidden-sm-and-down>
+      <v-card>
+        <v-container fluid fill-height>
+          <v-layout align-center justify-space-around fill-height>
+            <v-flex md6 style="padding-right: 12px;">
+              <img src="https://via.placeholder.com/200x200">
+            </v-flex>
+            <v-flex md6 style="padding-left: 12px;">
+              <!-- Logo Text -->
+              <h5 class="headline text-xs-center">AgradeSER</h5>
+              <br/>
+              <!-- Titulo -->
+              <h6 class="title text-xs-center">Vamos criar uma nova conta!</h6>
+              <!-- Email -->
+              <v-text-field
+                v-model="email"
+                type="email"
+                placeholder="Email"
+                prepend-icon="fas fa-at"
+              ></v-text-field>
+              <!-- Senha -->
+              <v-text-field
+                v-model="password"
+                :type="showPassword ? 'text' : 'password'"
+                placeholder="Senha"
+                prepend-icon="fas fa-key"
+                :append-icon="showPassword ? 'visibility_off' : 'visibility'"
+                @click:append="showPassword = !showPassword"
+              ></v-text-field>
+              <br>
+              <v-btn round block color="primary" dark v-on:click="signUp">Cadastre-se</v-btn>
+              <br>
+              <p class="text-xs-center">
+                Já tem uma conta?
+                <br>
+                Você pode fazer
+                <router-link :to="{ name: 'login' }">login</router-link>
+              </p>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-card>
+    </v-layout>
+
+    <!-- SM and Down -->
+    <v-layout align-center justify-center fill-height hidden-md-and-up>
+      <v-card>
+        <v-container fluid>
+          <v-layout align-center justify-space-around column fill-height>
+            <v-flex xs12>
+              <img src="https://via.placeholder.com/150x150">
+            </v-flex>
+            <v-flex xs12 style="margin-top: 10px;">
+              <!-- Logo Text -->
+              <h5 class="headline text-xs-center">AgradeSER</h5>
+              <br/>
+              <!-- Titulo -->
+              <h6 class="title text-xs-center">Vamos criar uma nova conta!</h6>
+              <!-- Email -->
+              <v-text-field
+                v-model="email"
+                type="email"
+                placeholder="Email"
+                prepend-icon="fas fa-at"
+              ></v-text-field>
+              <!-- Senha -->
+              <v-text-field
+                v-model="password"
+                :type="showPassword ? 'text' : 'password'"
+                placeholder="Senha"
+                prepend-icon="fas fa-key"
+                :append-icon="showPassword ? 'visibility_off' : 'visibility'"
+                @click:append="showPassword = !showPassword"
+              ></v-text-field>
+              <br>
+              <v-btn round block color="primary" dark v-on:click="signUp">Cadastre-se</v-btn>
+              <br>
+              <p class="text-xs-center">
+                Já tem uma conta?
+                <br>
+                Você pode fazer
+                <router-link :to="{ name: 'login' }">login</router-link>
+              </p>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-card>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
   import firebase from 'firebase'
+  import Trianglify from 'trianglify'
 
   export default {
     name: 'SignUp',
@@ -32,6 +115,18 @@
           }
         );
       }
+    },
+
+    created() {
+      // Background
+      const colors = ["#ef5f6f", "efa65d", "#f85467", "#f8a554"];
+      const pattern = Trianglify({
+        width: window.innerWidth,
+        height: window.innerHeight,
+        x_colors: colors,
+        cell_size: 60
+      });
+      this.background = pattern.png();
     }
   }
 </script>
