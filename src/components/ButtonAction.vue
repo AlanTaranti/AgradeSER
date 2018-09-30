8eb82e0e5d19c8e32c1afeaa8ec16714628277bd
Alan Taranti <alan.taranti@gmail.com>
2018-09-30 12:22:31 -0400

    formatado componente

--- src/components/ButtonAction.vue
+++ src/components/ButtonAction.vue
@@ -1,26 +1,26 @@
 <template>
-    <v-btn
-            fixed
-            bottom
-            right
-            dark
-            fab
-            color="accent"
-            @click="botaoClicado">
-        <v-icon>{{ icone }}</v-icon>
-    </v-btn>
+  <v-btn
+    fixed
+    bottom
+    right
+    dark
+    fab
+    color="accent"
+    @click="botaoClicado">
+    <v-icon>{{ icone }}</v-icon>
+  </v-btn>
 </template>
 
 <script>
-    export default {
-      name: 'button-action',
-      props: ['icone'],
-      methods: {
-        botaoClicado() {
-          this.$emit('botao-clicado');
-        },
+  export default {
+    name: 'button-action',
+    props: ['icone'],
+    methods: {
+      botaoClicado() {
+        this.$emit('botao-clicado');
       },
-    };
+    },
+  };
 </script>
 
 <style scoped>
